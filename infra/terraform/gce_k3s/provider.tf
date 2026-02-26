512c7d4b326a5b72ca7ccd7f8bf6a51960cabc20
dlwlsdnr <dlwlsdnr24@gmail.com>
2026-02-26 19:55:28 +0900

    add infra_terraform.yml

--- infra/terraform/gce_k3s/provider.tf
+++ infra/terraform/gce_k3s/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google ={
+      source = "hashicorp/google"
+      version = "~> 6.20"
+    }
+  }
+}
 provider "google" {
   project = var.project_id
   region  = var.region
